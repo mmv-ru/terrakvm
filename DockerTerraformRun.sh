15ab63a7f3eb5c457f731c1140cd4c231f7dcd53
MMV <mmv.rus@gmail.com>
2019-03-12 16:47:12 +0300

    make sudo inside of script
    sudo allow only system path or fullname

--- DockerTerraformRun.sh
+++ DockerTerraformRun.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-docker run --privileged -it --rm \
+sudo docker run --privileged -it --rm \
       -v /var/run/libvirt/libvirt-sock:/libvirt-sock \
       -v $PWD:/terraform \
       --volume $SSH_AUTH_SOCK:/ssh-agent --env SSH_AUTH_SOCK=/ssh-agent \
